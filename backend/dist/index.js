0a1960103b8db9694a5133d1a1418f7a6e4e4326
Afrin Nahar <afrinnahar1999@gmail.com>
2024-01-12 22:54:31 +0600

    configured basic server

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("Hello world!");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = require("http");
+const app = (0, express_1.default)();
+const server = (0, http_1.createServer)(app);
+app.get("/", (req, res) => {
+    console.log("Hello world!");
+});
+server.listen(3000, () => {
+    console.log("Server running on 3000");
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+import express from "express";
+import { createServer } from "http";
+
+const app = express();
+const server = createServer(app);
+
+app.get("/", (req, res) => {
+  console.log("Hello world!");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on 3000");
+});
