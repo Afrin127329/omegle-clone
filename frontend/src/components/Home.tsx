5e1c007d1d2e997f84ce6af5c50592bba1f3a697
Afrin Nahar <afrinnahar1999@gmail.com>
2024-01-23 23:23:08 +0600

    CSS added for lobby

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const Home = () => {
+  const userVideoRef = useRef();
+  const userOnlineVideoRef = useRef();
   const [name, setName] = useState("");
   return (
-    <div className="parent">
-      <video width={400} height={400} controls />
-      <input type="text" onChange={(e) => setName(e.target.value)} />
-      <button>Join Room</button>
+    <div className="container">
+      <h1>Welcome to Chat Room</h1>
+      <div className="video">
+        <video width={400} height={400} controls ref={userOnlineVideoRef} />
+        <video width={400} height={400} controls ref={userVideoRef} />
+      </div>
+      <div className="btn">
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter your name"
+        />
+        <button>Join Room</button>
+      </div>
     </div>
   );
 };
